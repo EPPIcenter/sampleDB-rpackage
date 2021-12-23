124fc27455c4c5955ebca58ac722036e4a079e37
seve-villabio <severianovillarruel@villa-bio.com>
2021-12-22 22:35:17 -0800

    made upload fun more logical but have not tested. may need to move to a new branch for testing

--- R/UploadSamples.R
+++ R/UploadSamples.R
@@ -4,21 +4,25 @@
 # UploadSamples <- function(barcode_file, barcode_type, longitudinal, plate_id, location){
 UploadSamples <- function(barcode_file, barcode_type, longitudinal, plate_id, location, study_short_code){
 
+  #OBTAIN TABLES AS THEY ARE IN THE DATABASE RIGHT NOW (SNAPSHOT)
   table.location <- sampleDB::CheckTable("location")
   table.study <- sampleDB::CheckTable("study")
   table.specimen_type <- sampleDB::CheckTable("specimen_type")
-  table.matrix_plate <- sampleDB::CheckTable("matrix_plate")
-  table.study_subject <- sampleDB::CheckTable("study_subject")
-  table.specimen <- sampleDB::CheckTable("specimen")
+  # table.matrix_plate <- sampleDB::CheckTable("matrix_plate")
+  # table.study_subject <- sampleDB::CheckTable("study_subject")
+  # table.specimen <- sampleDB::CheckTable("specimen")
 
-  # read in user supplied csv
+  #READIN CSV FROM USER WITH VISIONMATE/TRAXER BARCODES,
   if(barcode_type == "traxer"){
+
     csv <- read_csv(barcode_file) %>%
       drop_na() %>%
       mutate(barcode = `Tube ID`,
              well_position = paste0(substring(Position, 1, 1), substring(Position, 2))) %>%
       select(-c(Position:Date))
+
   }else{
+
     csv <- read_csv(barcode_file) %>%
       drop_na() %>%
       mutate(barcode = TubeCode,
@@ -26,89 +30,75 @@ UploadSamples <- function(barcode_file, barcode_type, longitudinal, plate_id, lo
       select(-c(LocationRow, LocationColumn, TubeCode))
   }
 
-  # adding data to the csv
-  csv <- csv %>% mutate(location = location,
-                        plate_id = plate_id,
-                        study_short_code = study_short_code)
-
-  # MATRIX_PLATE: add plate_id & location
-  location_id <- table.location %>% filter(description == location) %>% dplyr::pull(id)
+  #ADD PLATE_ID AND FREEZER LOCATION TO _*_MATRIX PLATE_*_ TABLE
   sampleDB::AddToTable("matrix_plate",
                        list(created = "dummy",
                             last_updated = "dummy",
                             uid = plate_id,
                             hidden = 0,
-                            location_id = location_id))
+                            location_id = table.location %>% filter(description == location) %>% dplyr::pull(id)))
 
-  # MATRIX_TUBE: add matrix_plate_id & barcode & well position
-  plate_id <- sampleDB::CheckTable("matrix_plate") %>% tail(1) %>% dplyr::pull(id)
+  #ADD PLATE_ID, BARCODE AND WELL POSITION TO _*_MATRIX TUBE_*_ TABLE
   for(i in 1:nrow(csv)){
-    barcode <- csv %>% slice(i) %>% dplyr::pull(barcode) %>% as.character
-    print(barcode)
-    well_position <- csv %>% slice(i) %>% dplyr::pull(well_position)
-    print(well_position)
-
     sampleDB::AddToTable("matrix_tube",
-                         list(plate_id = plate_id,
-                              barcode = barcode,
-                              well_position = well_position))
+                         list(plate_id = sampleDB::CheckTable("matrix_plate") %>% tail(1) %>% dplyr::pull(id),
+                              barcode = csv %>% slice(i) %>% dplyr::pull(barcode) %>% as.character(),
+                              well_position = csv %>% slice(i) %>% dplyr::pull(well_position)))
   }
 
-  #STUDY_SUBJECT: add uid & study_id (first check if uid is already in table)
-  study_id <- table.study %>% filter(short_code == study_short_code) %>% dplyr::pull(id)
-
+  #IF THE INDIVIDUAL_ID DOES NOT EXIST ADD IT TO THE _*_STUDY_SUBJECT_*_ TABLE
   for(i in 1:nrow(csv)){
 
-    uid <- csv %>% slice(i) %>% dplyr::pull(uid)
+    #CHECKING TO SEE IF INDIE ID EXISTS
+    if(csv[i, "individual_id"] %in% CheckTable("study_subject")$uid){
 
-    switch.subject_study_id <- FALSE
-    if(uid %in% (table.study_subject %>% dplyr::pull(uid))){
-      study_subject_id <- table.study_subject %>% filter(uid == uid) %>% dplyr::pull(id)
-      switch.subject_study_id <- TRUE
+      #IF INDIE ID EXISTS FETCH STUDY_SUBJECT_TABLE_ID ASSO W IT
+      study_subject_table_id <- CheckTable("study_subject") %>% filter(uid == csv[i, "individual_id"]) %>% dplyr::pull(id)
+
+    #INDIE ID IS NEW
     }else{
+
+      #CREATE A NEW STUDY_STUBJECT_TABLE ENTRY
       sampleDB::AddToTable("study_subject",
                            list(created = "dummy",
                                 last_updated = "dummy",
-                                uid = uid,
-                                study_id = study_id))
-      study_subject_id <- CheckTable("study_subject") %>% tail(1) %>% dplyr::pull(id)
+                                uid = csv %>% slice(i) %>% dplyr::pull(individual_id),
+                                study_id = CheckTable("study") %>% filter(short_code == study_short_code) %>% dplyr::pull(id)))
+
+      #FETCH THE NEW STUDY_SUBJECT_TABLE_ID
+      study_subject_table_id <- CheckTable("study_subject") %>% tail(1) %>% dplyr::pull(id)
     }
 
-    specimen_type_id <- table.specimen_type %>% filter(label == csv %>% slice(i) %>% dplyr::pull(specimen_type)) %>% dplyr::pull(id)
+    #ADD TO  _*_SPECIMEN_*_ TABLE AND _*_STORAGE_CONTAINER_*_ TABLE
 
-    #if the study_subject entry already exists;then use the specimen id
-    if(switch.subject_study_id){
+    #CHECKING TO SEE IF INDIE EXISTS; IF INDIE EXISTS SPECIMEN_ID EXISTS
+    if(csv[i, "individual_id"] %in% CheckTable("study_subject")$uid){
 
-      print("subject_study_id already exists")
-      specimen_id <- table.specimen %>% filter(study_subject_id == study_subject_id, specimen_type_id == specimen_type_id) %>% dplyr::pull(id)
-
+      #ADD SPECIMEN_ID TO  _*_STORAGE_CONTAINER_*_ TABLE IF STUDY_SUBJECT ENTRY EXISTS
       sampleDB::AddToTable("storage_container",
                            list(created = "dummy",
                                 last_updated = "dummy",
                                 type = NA,
-                                specimen_id = specimen_id,
+                                specimen_id = CheckTable("specimen") %>% filter(study_subject_id == study_subject_table_id, specimen_type_id == filter(table.specimen_type, label == csv[i, "specimen_type"])$id) %>% dplyr::pull(id),
                                 comments = NA,
                                 exhausted = 0))
     }else{
 
-      print("subject_study_id DOES NOT exists")
-      #SPECIMEN TABLE
+      #ADD STUDY_SUBJECT_ID AND SPECIMEN_TYPE_ID TO _*_SPECIMEN_TABLE_*_ AND ADD THE NEW SPECIMEN_ID TO _*_STORAGE_CONTAINER_*_ TABLE IF SUBJECT_STUDY ENTRY DOES NOT EXIST
       sampleDB::AddToTable("specimen",
                            list(created = "dummy",
                                 last_updated = "dummy",
-                                study_subject_id = study_subject_id,
-                                specimen_type_id = specimen_type_id,
+                                study_subject_id = study_subject_table_id,
+                                specimen_type_id = filter(table.specimen_type, label == csv[i, "specimen_type"])$id,
                                 collection_date = NA))
     }
-
-    specimen_id <- CheckTable("specimen") %>% tail(1) %>% dplyr::pull(id)
 
     #STORAGE CONTAINER TABLE
     sampleDB::AddToTable("storage_container",
                          list(created = "dummy",
                               last_updated = "dummy",
                               type = NA,
-                              specimen_id = specimen_id,
+                              specimen_id = CheckTable("specimen") %>% tail(1) %>% dplyr::pull(id),
                               comments = NA,
                               exhausted = 0))
   }
