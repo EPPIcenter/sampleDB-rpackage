25491709d6f4be1c25ed5281bb0d291182694f7a
Severiano Villarruel <villars4@Aragorn>
2022-02-14 14:54:36 -0800

    set up empty container deleting

---
+++ R/DeleteEmptyContainer.R
@@ -0,0 +1,53 @@
+#' @import dplyr
+#' @export
+
+DeleteEmptyPlates <- function(database, type, container_name){
+  
+  stopifnot("Sample Type is not valid" = type %in% c("micronix", "cryovile", "rdt", "paper"))
+  if(type == "micronix"){
+    id.container <- filter(sampleDB::CheckTable(database = database, "matrix_plate"), plate_name == container_name)$id
+    if(filter(sampleDB::CheckTable("matrix_tube"), plate_id %in% id.container) %>% nrow() == 0){
+      sampleDB::DeleteFromTable(database = database, 
+                                table_name = "matrix_plate", 
+                                id = as.character(id.container))  
+      message(paste0("Successfully Deleted Container: \n", container_name))
+    }else{
+      message("Error Contianer is not empty")
+    }
+  }
+  else if(type == "cryovile"){
+    id.container <- filter(sampleDB::CheckTable(database = database, "box"), box_name == container_name)$id
+    if(filter(sampleDB::CheckTable("tube"), plate_id %in% id.container) %>% nrow() == 0){
+      sampleDB::DeleteFromTable(database = database, 
+                                table_name = "box", 
+                                id = as.character(id.container))  
+      message(paste0("Successfully Deleted Container: \n", container_name))
+    }else{
+      message("Error Contianer is not empty")
+    }
+  }
+  else if(type == "rdt"){
+    id.container <- filter(sampleDB::CheckTable(database = database, "bag"), bag_name == container_name)$id
+    if(filter(sampleDB::CheckTable("rdt"), bag_id %in% id.container) %>% nrow() == 0){
+      sampleDB::DeleteFromTable(database = database, 
+                                table_name = "bag", 
+                                id = as.character(id.container)) 
+      message(paste0("Successfully Deleted Container: \n", container_name))
+    }else{
+      message("Error Contianer is not empty")
+    }
+  }
+  else{
+    id.container <- filter(sampleDB::CheckTable(database = database, "bag"), bag_name == container_name)$id
+    if(filter(sampleDB::CheckTable("paper"), bag_id %in% id.container) %>% nrow() == 0){
+      sampleDB::DeleteFromTable(database = database, 
+                                table_name = "bag", 
+                                id = as.character(id.container))
+      message(paste0("Successfully Deleted Container: \n", container_name))
+    }else{
+      message("Error Contianer is not empty")
+    }
+  }
+  
+  message(paste0("Successfully Deleted Container: \n", container_name))
+}
