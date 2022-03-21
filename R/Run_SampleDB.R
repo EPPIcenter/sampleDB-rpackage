c9be5946e5e208713a2368fd434162b6352a0f94
Severiano Villarruel <severiano.villarruel@ucsf.edu>
2022-03-21 10:34:06 -0700

    Generate a SDB backup each time the app is run
    This does not necessarily create a back up after each move or upload,
    bc these funs can be used outside of the app.
    Backups are also generated twice a day, midnight and noon.
    UploadCSVs and Upload Moves should be stored.
    And other measures may need to be taken in order to ensure backing up
    the DB is robust.

--- R/Run_SampleDB.R
+++ R/Run_SampleDB.R
@@ -16,5 +16,7 @@
 #' @export
 
 Run_SampleDB <- function(){
+  # make a copy of the database before each run
+  system("if test -f \"/bin/sampleDB_backup_generator.sh\"; then bash /bin/sampleDB_backup_generator.sh; fi")
   shiny::runApp(system.file('sampleDB', package='sampleDB'))
 }
