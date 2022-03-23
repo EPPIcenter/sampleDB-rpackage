c1f6b3a06fa8f088d28031fcaecbcac5ff58dbc6
Severiano Villarruel <severiano.villarruel@ucsf.edu>
2022-03-22 17:39:06 -0700

    Create a function to set up sampleDB
    This function doesn't work well bc the global .Renviron-site is
    protected from editing. This commit is actually a work in progress...

---
+++ R/SampleDB_Setup.R
@@ -0,0 +1,25 @@
+#' Setup SampleDB Database
+#' @import dplyr
+#' @export
+#' 
+
+
+SampleDB_Setup <- function(){
+  
+  # ask user where to store database file
+  path <- readline("Please provide a readable and writable path to store the database: ")
+  
+  # move database to user specified path
+  sqlite_file <- system.file("extdata", "sampledb_database.sqlite", package = "sampleDB")
+  system(paste("cp", sqlite_file, path))
+  Sys.chmod(paste0(path, "/", sqlite_file), mode = "0777")
+  
+  # user message
+  message(paste0("SampleDB database file now exists at:\n", path, "/", sqlite_file))
+  
+  #add variable to .renvirons-site using linux
+  #if error is encountered counsel the user to either create .renvirons or make it readable or move it out of admin zone
+  
+  
+  return()
+}
