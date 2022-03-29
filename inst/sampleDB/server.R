f4ef473b8d333e160661e7d20c9e56a8d7c3a79f
Severiano Villarruel <severiano.villarruel@ucsf.edu>
2022-03-29 15:33:29 -0700

    First 'establish_permissions_db_setup' branch commit

--- inst/sampleDB/server.R
+++ inst/sampleDB/server.R
@@ -16,6 +16,45 @@ function(input, output, session) {
     # SET PATH TO SQLITE DATABASE - WOULD PREFER DATABASE TO BE AT Sys.getenv("SAMPLEDB_DATABASE")
     database <- Sys.getenv("SDB_PATH")
 
+    # reactiveValues object for storing current data set.
+    vals <- reactiveValues(data = NULL)
+    
+    # Return the UI for a modal dialog with data selection input. If 'failed' is
+    # TRUE, then display a message that the previous value was invalid.
+    dataModal <- function(failed = FALSE){
+      modalDialog(
+        textInput("dataset", "It looks like SampleDB has not yet been setup on your computer",
+                  placeholder = 'password'
+        ),
+        span('In order to setup SampleDB on this computer please provide the server admin password'),
+        if (failed)
+          div(tags$b("Invalid name of data object", style = "color: red;")),
+        
+        footer = tagList(
+          modalButton("Cancel"),
+          actionButton("ok", "OK")
+        )
+      )
+    }
+    
+    showModal(dataModal())
+    
+    # When OK button is pressed, attempt to load the data set. If successful,
+    # remove the modal. If not show another modal, but this time with a failure
+    # message.
+    observeEvent(input$ok, {
+      print(input$dataset)
+      # Check that data object exists and is data frame.
+      if (!is.null(input$dataset) && nzchar(input$dataset) &&
+          exists(input$dataset) && is.data.frame(get(input$dataset))) {
+        vals$data <- get(input$dataset)
+        removeModal()
+      } else {
+        showModal(dataModal(failed = TRUE))
+      }
+    })
+
+    
     # --------- Upload Samples -------------
 
     # Upload Micronix Samples
